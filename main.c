b3ddc59215cc9b6bce168d175d900260e77a0ed9
Andrei Hirata <69882538+andreihirata@users.noreply.github.com>
2021-04-27 16:45:41 -0300

    Adicionadas as funções finais
    Projeto finalizado

--- main.c
+++ main.c
@@ -7,13 +7,6 @@
 #include <stdio.h>
 #include <time.h>
 
-//#include <sys/resource.h>
-#include <windows.h>
-#include <stdlib.h>
-#include <errno.h>
-//#include <unistd.h>
-#include <psapi.h>
-
 #include "pilha/contigua/pilhaContigua.h"
 #include "pilha/contigua/pilhaContigua.c"
 
@@ -36,8 +29,44 @@ void printInteiro(void* n){
 }
 
 //---------------------------------<Funçôes Contigua>-------------------------------------------//
-void basic_function_contigua_empilhar_imprimir(int value){
-    printf(">>> TESTE EMPILHAR\n");
+void function_contigua_empilhar(int value){
+    TICK(TIME_A);
+    printf(">>> TESTE EMPILHAR CONTIGUA\n");
+    PilhaContigua* p = pilha_contigua_criar();
+    for(int i=0;i<value;i++){
+        pilha_contigua_empilhar(p, i);
+    }
+    pilha_contigua_destruir(&p);
+    TOCK(TIME_A);
+}
+
+void function_contigua_desempilhar(int value){
+    printf(">>> TESTE EMPILHAR CONTIGUA\n");
+    PilhaContigua* p = pilha_contigua_criar();
+    for(int i=0;i<value;i++){
+        pilha_contigua_empilhar(p, i);
+    }
+    TICK(TIME_A);
+    printf(">>> TESTE DESEMPILHAR CONTIGUA\n");
+
+    int elemento;
+    for(int i=0;i<value;i++){
+        pilha_contigua_desempilhar(p, &elemento);
+        int resultado = pilha_contigua_desempilhar(p, &elemento);
+        if (!resultado){
+            break;
+        }
+    }
+    pilha_contigua_destruir(&p);
+    TOCK(TIME_A);
+}
+
+
+
+void combination_function_contigua_empilhar_imprimir(int value){
+    TICK(TIME_A);
+    printf(">>> TESTE EMPILHAR CONTIGUA\n");
+
     PilhaContigua* p = pilha_contigua_criar();
     for(int i=0;i<value;i++){
         pilha_contigua_empilhar(p, i);
@@ -46,15 +75,17 @@ void basic_function_contigua_empilhar_imprimir(int value){
     pilha_contigua_imprimir(p, printInteiro);
     printf("\n");
     pilha_contigua_destruir(&p);
+    TOCK(TIME_A);
 }
 
-void basic_function_contigua_empilhar_desempilhar_imprimir(int value){
-    printf(">>> TESTE EMPILHAR\n");
+void combination_function_contigua_empilhar_desempilhar_imprimir(int value){
+    TICK(TIME_A);
+    printf(">>> TESTE EMPILHAR CONTIGUA\n");
     PilhaContigua* p = pilha_contigua_criar();
     for(int i=0;i<value;i++){
         pilha_contigua_empilhar(p, i);
     }
-    printf(">>> TESTE DESEMPILHAR\n");
+    printf(">>> TESTE DESEMPILHAR CONTIGUA\n");
     int elemento;
     for(int i=0;i<value;i++){
         pilha_contigua_desempilhar(p, &elemento);
@@ -67,14 +98,48 @@ void basic_function_contigua_empilhar_desempilhar_imprimir(int value){
 
     }
     pilha_contigua_destruir(&p);
+    TOCK(TIME_A);
 }
 
 //---------------------------------</Funçôes Contigua>-------------------------------------------//
 
 
 //---------------------------------<Funçôes Encadeadas>-------------------------------------------//
-void basic_function_encadeada_empilhar_imprimir(int value){
-    printf(">>> TESTE EMPILHAR\n");
+void function_encadeada_empilhar(int value){
+    TICK(TIME_A);
+    printf(">>> TESTE EMPILHAR ENCADEADA\n");
+    PilhaEncadeada* p = pilha_encadeada_criar();
+    for(int i=0;i<value;i++){
+        pilha_encadeada_empilhar(p, i);
+    }
+    pilha_encadeada_destruir(p);
+    TOCK(TIME_A);
+}
+
+void function_encadeada_desempilhar(int value){
+    TICK(TIME_A);
+    printf(">>> TESTE EMPILHAR ENCADEADA\n");
+    PilhaEncadeada* p = pilha_encadeada_criar();
+    for(int i=0;i<value;i++){
+        pilha_encadeada_empilhar(p, i);
+    }
+    printf(">>> TESTE DESEMPILHAR ENCADEADA\n");
+    int elemento;
+    for(int i=0;i<value;i++){
+        pilha_encadeada_desempilhar(p, &elemento);
+        int resultado = pilha_encadeada_desempilhar(p, &elemento);
+        if (!resultado){
+            break;
+        }
+
+    }
+    pilha_encadeada_destruir(p);
+    TOCK(TIME_A);
+}
+
+void combination_function_encadeada_empilhar_imprimir(int value){
+    TICK(TIME_A);
+    printf(">>> TESTE EMPILHAR ENCADEADA\n");
     PilhaEncadeada* p = pilha_encadeada_criar();
     for(int i=0;i<value;i++){
         pilha_encadeada_empilhar(p, i);
@@ -83,34 +148,85 @@ void basic_function_encadeada_empilhar_imprimir(int value){
     pilha_encadeada_imprimir(p);
     printf("\n");
     pilha_encadeada_destruir(p);
+    TOCK(TIME_A);
+}
+
+void combination_function_encadeada_empilhar_desempilhar_imprimir(int value){
+    TICK(TIME_A);
+    printf(">>> TESTE EMPILHAR ENCADEADA\n");
+    PilhaEncadeada* p = pilha_encadeada_criar();
+    for(int i=0;i<value;i++){
+        pilha_encadeada_empilhar(p, i);
+    }
+    printf(">>> TESTE DESEMPILHAR ENCADEADA\n");
+    int elemento;
+    for(int i=0;i<value;i++){
+        pilha_encadeada_desempilhar(p, &elemento);
+        printf("Removido: %d\n", elemento);
+        int resultado = pilha_encadeada_desempilhar(p, &elemento);
+        if (!resultado){
+            printf("Pilha vazia\n");
+            break;
+        }
+
+    }
+    pilha_encadeada_destruir(p);
+    TOCK(TIME_A);
 }
 //---------------------------------</Funçôes Encadeadas>-------------------------------------------//
+
+//Menu
+
+
 int main(){
 
-    TICK(TIME_A);
 
-    //-------------------------Teste Pilha de Alocação Contígua / Funções Básicas-------------------------------------//
-    //basic_function_contigua_empilhar_imprimir(dado10);
-    //basic_function_contigua_empilhar_imprimir(dado50);
-    //basic_function_contigua_empilhar_imprimir(dado100);
-    //basic_function_contigua_empilhar_imprimir(dado500);
+    //-------------------------Teste Pilha de Alocação Contígua-------------------------------------//
+    //function_contigua_empilhar(dado10);
+    //function_contigua_empilhar(dado50);
+    //function_contigua_empilhar(dado100);
+    //function_contigua_empilhar(dado500);
 
-    //basic_function_contigua_empilhar_desempilhar_imprimir((dado10));
-    //basic_function_contigua_empilhar_desempilhar_imprimir((dado50));
-    //basic_function_contigua_empilhar_desempilhar_imprimir((dado100));
-    //basic_function_contigua_empilhar_desempilhar_imprimir((dado500));
+    //function_contigua_desempilhar(dado10);
+    //function_contigua_desempilhar(dado50);
+    //function_contigua_desempilhar(dado100);
+    //function_contigua_desempilhar(dado500);
 
 
-    //basic_function_encadeada_empilhar_imprimir(dado10);
-    //basic_function_encadeada_empilhar_imprimir(dado50);
-    //basic_function_encadeada_empilhar_imprimir(dado100);
-    basic_function_encadeada_empilhar_imprimir(dado500);
+    //combination_function_contigua_empilhar_imprimir(dado10);
+    //combination_function_contigua_empilhar_imprimir(dado50);
+    //combination_function_contigua_empilhar_imprimir(dado100);
+    //combination_function_contigua_empilhar_imprimir(dado500);
 
-    TOCK(TIME_A);
+    //combination_function_contigua_empilhar_desempilhar_imprimir((dado10));
+    //combination_function_contigua_empilhar_desempilhar_imprimir((dado50));
+    //combination_function_contigua_empilhar_desempilhar_imprimir((dado100));
+    //combination_function_contigua_empilhar_desempilhar_imprimir((dado500));
 
-    //TICK(TIME_B);
-    //functionB();
-    //TOCK(TIME_B);
+
+    //-------------------------Teste Pilha de Alocação Encadeada------------------------------------//
+    //function_encadeada_empilhar(dado10);
+    //function_encadeada_empilhar(dado50);
+    //function_encadeada_empilhar(dado100);
+    //function_encadeada_empilhar(dado500);
+
+    //Problema aqui
+    //function_encadeada_desempilhar(dado10);
+    //function_encadeada_desempilhar(dado50);
+    //function_encadeada_desempilhar(dado100);
+    //function_encadeada_desempilhar(dado500);
+
+
+    //combination_function_encadeada_empilhar_imprimir(dado10);
+    //combination_function_encadeada_empilhar_imprimir(dado50);
+    //combination_function_encadeada_empilhar_imprimir(dado100);
+    //combination_function_encadeada_empilhar_imprimir(dado500);
+
+    //Problema aqui
+    //combination_function_encadeada_empilhar_desempilhar_imprimir((dado10));
+    //combination_function_encadeada_empilhar_desempilhar_imprimir((dado50));
+    //combination_function_encadeada_empilhar_desempilhar_imprimir((dado100));
+    combination_function_encadeada_empilhar_desempilhar_imprimir((dado500));
 
 
     return 0;
